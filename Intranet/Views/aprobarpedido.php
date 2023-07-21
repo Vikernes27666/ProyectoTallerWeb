b1bef3d5a2a07e4aac4f72ed14e423760bf5d6cf
vikernes <armic27666@gmail.com>
2023-07-21 17:28:29 -0500

    new

--- Intranet/Views/aprobarpedido.php
+++ Intranet/Views/aprobarpedido.php
@@ -5,12 +5,83 @@ require '../Model/_db.php';
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Eliminar el código de actualización del estado del pedido
+    // Consulta a la base de datos para obtener los datos del pedido según el ID
+    $sql = "SELECT * FROM pago WHERE id = '$id'";
+    $result = mysqli_query($conexion, $sql);
 
-    echo "El pedido se ha aprobado correctamente.";
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+    } else {
+        // Si no se encuentra el pedido con el ID proporcionado, redirigimos al usuario o mostramos un mensaje
+        echo "<script>alert('El pedido con ID $id no fue encontrado.'); window.location.href='ruta_a_otra_pagina.php';</script>";
+        exit;
+    }
+
+    mysqli_close($conexion);
 } else {
-    echo "ID de pedido no válido.";
+    // Si no hay ID de pedido válido, redirigimos al usuario o mostramos un mensaje
+    echo "<script>alert('ID de pedido no válido.'); window.location.href='ruta_a_otra_pagina.php';</script>";
+    exit;
 }
-
-mysqli_close($conexion);
 ?>
+
+<!-- HTML Modal -->
+<div id="myModal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <!-- Mostrar los campos correspondientes aquí -->
+    <h3>Información del pedido:</h3>
+    <p>Delivery Option: <?php echo $row['delivery_option']; ?></p>
+    <p>Address: <?php echo $row['address']; ?></p>
+    <p>First Name: <?php echo $row['first_name']; ?></p>
+    <p>Last Name: <?php echo $row['last_name']; ?></p>
+    <p>Email: <?php echo $row['email']; ?></p>
+    <p>Phone Number: <?php echo $row['phone_number']; ?></p>
+    <p>Amount: <?php echo $row['amount']; ?></p>
+
+    <h3>Seleccione el estado del pedido:</h3>
+    <form id="emailForm">
+      <select name="orderStatus">
+        <option value="preparado">Su pedido está siendo preparado.</option>
+        <option value="en_camino">Su pedido está en camino.</option>
+        <option value="entregado">Su pedido ha sido entregado con éxito.</option>
+      </select>
+      <input type="hidden" name="pedidoId" value="<?php echo $_GET['id']; ?>">
+      <button type="submit">Enviar correo</button>
+    </form>
+  </div>
+</div>
+
+<!-- JavaScript for the Modal -->
+<script src="https://ruta_del_cdn.com/mailto-ui.min.js"></script>
+<script>
+  // Resto del código JavaScript como antes...
+  const modal = document.getElementById("myModal");
+  const span = document.getElementsByClassName("close")[0];
+  const emailForm = document.getElementById("emailForm");
+
+  span.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  emailForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const selectedStatus = emailForm.orderStatus.value;
+    const pedidoId = emailForm.pedidoId.value;
+    if (pedidoId && selectedStatus) {
+      // Utiliza la librería MailtoUI para enviar el correo electrónico con el estado seleccionado y el contenido del pedido.
+      // Aquí deberás ajustar el código para enviar el correo con la información necesaria.
+      window.location.href = `mailto:mario@teclaworks.com?subject=Actualización de Pedido ID ${pedidoId}&body=El estado del pedido con ID ${pedidoId} ha sido actualizado a: ${selectedStatus}`;
+      modal.style.display = "none";
+      alert("Correo electrónico enviado correctamente.");
+    } else {
+      alert("Por favor, seleccione un estado del pedido antes de enviar el correo.");
+    }
+  });
+</script>
--- php/pagos.php
+++ php/pagos.php
@@ -1,4 +1,8 @@
 <?php
+require_once '../libreria/dompdf/autoload.inc.php'; // Asegúrate de poner la ruta correcta hacia autoload.inc.php
+
+use Dompdf\Dompdf;
+
 // Obtener los datos del formulario
 $cardNumber = $_POST['cardNumber'];
 $cardName = $_POST['cardName'];
@@ -12,7 +16,7 @@ $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $amount = $_POST['amount'];
 
-// Realizar la conexión a la base de datos
+// Realizar la conexión a la base de datos (si es necesario)
 $servername = "localhost";
 $username   = "root";
 $password   = "";
@@ -20,21 +24,117 @@ $dbname     = "norkysbd";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
+// Verificar la conexión (si es necesario)
 if ($conn->connect_error) {
     die("La conexión a la base de datos ha fallado: " . $conn->connect_error);
 }
 
-// Insertar los datos en la base de datos
+// Insertar los datos en la base de datos (si es necesario)
 $sql = "INSERT INTO pago (card_number, card_name, expiry_date, cvv, delivery_option, address, email, phone_number, first_name, last_name, amount)
         VALUES ('$cardNumber', '$cardName', '$expiryDate', '$cvv', '$deliveryOption', '$address', '$email', '$phoneNumber', '$firstName', '$lastName', '$amount')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Los datos se han guardado correctamente en la base de datos.";
-} else {
-    echo "Error al guardar los datos en la base de datos: " . $conn->error;
+if ($conn->query($sql) !== TRUE) {
+    echo "Su compra no se ha realizado con éxito." . $conn->error;
+    $conn->close();
+    exit();
 }
 
-// Cerrar la conexión a la base de datos
-$conn->close();
+// La compra se ha realizado con éxito, procedemos a generar el contenido del PDF
+// Crear una instancia de Dompdf
+$dompdf = new Dompdf();
+
+// Crear el contenido del PDF con diseño
+$content = "<!DOCTYPE html>
+<html>
+<head>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+  }
+  .header {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .details {
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    padding: 10px;
+  }
+  .thanks {
+    text-align: center;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 30px;
+  }
+  .signature {
+    text-align: center;
+    font-size: 14px;
+    margin-top: 50px;
+  }
+</style>
+</head>
+<body>
+<div class='header'>
+  <h1 style='font-size: 24px; font-weight: bold;'>Norkys</h1>
+</div>
+<div class='details'>
+  <div style='font-size: 18px; font-weight: bold; margin-bottom: 10px;'>Boleta de Compra</div>
+  <b>Nombre:</b> $firstName $lastName<br>
+  <b>Correo electrónico:</b> $email<br>
+  <b>Número de teléfono:</b> $phoneNumber<br>
+  <b>Opción de entrega:</b> $deliveryOption<br>";
+
+if ($deliveryOption === 'delivery') {
+    // Agregar dirección de entrega si aplica
+    $content .= "<b>Dirección de entrega:</b> $address<br>";
+} else {
+    // Agregar información sobre el restaurante de recojo si aplica
+    $content .= "<b>Restaurante de recojo:</b> (Aquí podrías obtener el restaurante más cercano)<br>";
+}
+
+$content .= "<b>Monto a pagar:</b> $amount soles</div>
+<div class='thanks'>Gracias por su compra!</div>
+<div class='signature'>
+  ____________________________________<br>
+  Firma: " . generateRandomSignature() . "
+</div>
+</body>
+</html>";
+
+// Cargar el contenido HTML en Dompdf
+$dompdf->loadHtml($content);
+
+// Configurar las opciones del PDF (opcional)
+$dompdf->setPaper('A4', 'portrait');
+
+// Renderizar el contenido HTML en PDF
+$dompdf->render();
+
+// Generar un identificador único para el nombre del archivo PDF
+$unique_id = uniqid();
+$file = "Boleta_de_Compra_" . $unique_id . ".pdf";
+
+// Guardar el PDF en la ubicación deseada
+file_put_contents($file, $dompdf->output());
+
+// Descargar el archivo PDF
+header("Content-type: application/pdf");
+header("Content-Disposition: attachment; filename=" . $file);
+readfile($file);
+
+// Terminar la ejecución del script después de la descarga
+exit();
+
+// Función para generar una firma aleatoria
+function generateRandomSignature() {
+    $signatures = array(
+        "John Doe",
+        "Jane Smith",
+        "Robert Johnson",
+        "Mary Williams",
+        "Michael Brown"
+    );
+    $randomIndex = array_rand($signatures);
+    return $signatures[$randomIndex];
+}
 ?>
